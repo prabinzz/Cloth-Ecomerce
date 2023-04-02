182679b3c75950df9b430fcdabc2c11ff1e5b0b8
prabinzz <prabinparajuli92@gmail.com>
2023-04-02 18:00:02 +0545

    Return connection insted of  storing as attribute

--- src/main/java/database/Database.java
+++ src/main/java/database/Database.java
@@ -5,17 +5,17 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Database {
-	protected Connection conn;
-	public Database() {
+	public static Connection conn() {
 		String url = "jdbc:mysql://localhost:3306/cloth_ecom";
 		String username = "root";
 		String password = "";
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			conn = DriverManager.getConnection(url, username, password);
+			return DriverManager.getConnection(url, username, password);
 		} catch (ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return null;
 	}
 }
